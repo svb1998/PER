25af0dfc91667cfd140b35736580847347ed16d6
svb1998 <1998.svb@gmail.com>
2020-04-19 14:27:05 +0200

    Modificado mnn.m

--- Proyecto/mnn.m
+++ Proyecto/mnn.m
@@ -1,8 +1,18 @@
 %FUNCION MNN
 function [V] = mnn(X,xl,m)
+N=rows(X);
+
+numbatches=N*N*4/intmax*10;
+if (numbatches<1) numbatches=1; end
+
+batchsize=ceil(N/numbatches);
+
+
+
 
 for i=1:m
-	D = L2dist(X,i);
+	Xbatch=X((i-1)*batchsize+1:min(i*batchsize,N),:);
+	D = L2dist(X,Xbatch);
 	[D,idx]= sort(D,'ascend');
 	idx= idx(1:m,:);
 	V = [V mode(xl'(idx),1)];
