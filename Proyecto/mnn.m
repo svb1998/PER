7a52e28d3616612f311fb0abfc1e8ec87539b1ca
svb1998 <1998.svb@gmail.com>
2020-04-20 14:31:13 +0200

    wp-exp.m y mnn.m modificado

--- Proyecto/mnn.m
+++ Proyecto/mnn.m
@@ -4,13 +4,13 @@ N=rows(X);
 
 V = [];
 
-numbatches=N*N*4/intmax*10;
+numbatches=ceil(N*N*4/1024^3)*4;
 if (numbatches<1) numbatches=1; end
 
 batchsize=ceil(N/numbatches);
 
 
-for i=1:m
+for i=1:numbatches
 	Xbatch=X((i-1)*batchsize+1:min(i*batchsize,N),:);
 	D = L2dist(X,Xbatch);
 	[D,idx]= sort(D,'ascend');
