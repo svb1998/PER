b0c69d243bd8c26ec440aa3bacb6383232dd46ff
CarlosCobas <ccobas@siroppe.com>
2020-05-13 21:27:19 +0200

    Clasificador polinomial

---
+++ Proyecto/multinomial.m
@@ -0,0 +1,53 @@
+function [etr, edv]=multinomial(Xtr, xltr, Xdv, xldv, epsilons)
+
+    %1e-10 1e-9 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1
+    printf("Empezamos\n");
+    for e=epsilons  
+
+        %Matriz con la probabilidad a priori por clase
+        clas_prob = [];
+
+        %Matriz con prototipo polinomial de cada clase
+        prot_pol = [];
+
+        %Total de clases
+        N = length(xltr);  
+
+        %Clases selecinadas por el clasificador
+        cls = [];
+
+        for i = unique(xltr)'
+
+            %indices de elementos con clase i
+            ind_i = find(xltr==i);
+
+            %Total de elementos con clase i
+            cn = length(ind_i);
+
+            %Calcular porbabilidad de clase i
+            prob_c = cn / N;
+            clas_prob = cat(1, clas_prob, prob_c);
+        
+            %Obtener imagenes con clase i
+            Xtri = Xtr(ind_i, :);
+
+            %Prototipo Polinomial clase i
+            added_vector = sum(Xtri);
+            total_vector = sum(added_vector);
+            prot_pol_i = added_vector/total_vector; 
+
+            %normalizar prototipo
+            norm_prot = prot_pol_i + e / sum(prot_pol_i + e);
+
+            prot_pol = cat(1, prot_pol, norm_prot);
+        endfor
+
+        printf("Ya calculamos los terminos\n");
+
+
+        [val_max, i_max] = max(Xtr * log(prot_pol') + log(clas_prob), [], 2);
+        cls = i_max - 1
+        
+    endfor
+
+endfunction
